50084435f61b853699022864087b933b1eab046b
Bay Wiggins <pax8nw@virginia.edu>
2024-09-30 13:37:57 -0400

    Finished preparing data

--- NBAStats.py
+++ NBAStats.py
@@ -7,48 +7,6 @@ import dotenv
 import os
 import json
 
-YEARS = [2023]
-MONTHS = [1, 2, 3, 4, 5, 6, 10, 11, 12]
-# MONTHS = [6]
-END_DAY = 31
+x = client.player_box_scores(day=1, month=1, year=2017, output_type=OutputType.JSON)
 
-OUTPUT_TYPE = OutputType.JSON
-OUTPUT_FILE = 'data/output.csv'
-
-needs_header = False
-
-with open('data/def_rating.json') as file:
-    def_rating_data = json.load(file)
-
-def add_defensive_ratings(row):
-    team_def_rtg = def_rating_data[row['opponent']]
-    year = int(row['date'].split("-")[-1])
-    
-    for i in team_def_rtg:
-        if i['year'] == year:
-            return i['def_rtg'], i['def_rtg_adj']
-    
-    return -1, -1
-
-
-for year in YEARS:
-    for month in MONTHS:
-        for day in range(4, END_DAY + 1):
-            try:
-                j = json.loads(client.player_box_scores(day=day, month=month, year=year, output_type=OUTPUT_TYPE))
-                if not j:
-                    continue
-            except ParserError:
-                continue
-
-            df = pd.DataFrame(j)
-
-            df.drop(columns=["slug"], inplace=True)
-
-            df['date'] = f"{month}-{day}-{year}"
-
-            df[['def_rtg', 'def_rtg_adj']] = df.apply(add_defensive_ratings, axis=1, result_type="expand")
-            
-
-            df.to_csv(OUTPUT_FILE, mode='a', index=False, header=True if needs_header else False)
-            needs_header = False
+print(x)
